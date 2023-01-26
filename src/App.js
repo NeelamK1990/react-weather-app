8cd389adfc639cb7bbb01abe3a4b46136ba4a20a
NeelamK1990 <neelamkatwal606@gmail.com>
2023-01-26 15:34:00 -0500

    Added the search structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by Neelam Katwal and is{" "}
-        <a
-          href="https://github.com/NeelamK1990/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by Neelam Katwal and is{" "}
+          <a
+            href="https://github.com/NeelamK1990/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
