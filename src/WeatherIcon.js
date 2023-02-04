8cee70cde93001e2daa66c5f9ba4465909c63f37
NeelamK1990 <neelamkatwal606@gmail.com>
2023-02-04 12:46:57 -0500

    Added final css to my project

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#6088bb"
       size={props.size}
       animate={true}
     />
